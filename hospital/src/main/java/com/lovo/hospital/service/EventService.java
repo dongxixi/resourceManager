2443e2abb23b6c19d5c860d068bfb734221e8bd9
Daoyi <798464205@qq.com>
2019-04-19 17:59:41 +0800

    事件页面，service,serviceImpl

--- hospital/src/main/java/com/lovo/hospital/service/EventService.java
+++ hospital/src/main/java/com/lovo/hospital/service/EventService.java
@@ -1,5 +1,28 @@
 package com.lovo.hospital.service;
 
+import com.lovo.hospital.dto.EventRecordListDto;
+
+import java.util.List;
+
 public interface EventService {
 
+    /**
+     * 条件得到事件列表
+     * @param currPage 当前页
+     * @param eventName 事件名称
+     * @param startTime 条件开始时间
+     * @param endTime  条件结束时间
+     * @return
+     */
+    List<EventRecordListDto> getEventDtoList(Integer currPage, String eventName, String startTime, String endTime);
+
+    /**
+     * 总页数
+     * @param carNum 当前页
+     * @param eventName 事件名称
+     * @param startTime 条件开始时间
+     * @param endTime 条件结束时间
+     * @return
+     */
+    Integer getTotalPage(Integer carNum, String eventName, String startTime, String endTime);
 }
--- hospital/src/main/java/com/lovo/hospital/service/impl/EventServiceImpl.java
+++ hospital/src/main/java/com/lovo/hospital/service/impl/EventServiceImpl.java
@@ -1,8 +1,45 @@
 package com.lovo.hospital.service.impl;
 
+import com.lovo.hospital.dao.EventDao;
+import com.lovo.hospital.dto.EventRecordListDto;
 import com.lovo.hospital.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service(value = "eventService")
 public class EventServiceImpl implements EventService {
+    @Autowired
+    private EventDao eventDao;
+    /**
+     * 条件得到事件列表
+     *
+     * @param currPage  当前页
+     * @param eventName 事件名称
+     * @param startTime 条件开始时间
+     * @param endTime   条件结束时间
+     * @return
+     */
+    @Override
+    public List<EventRecordListDto> getEventDtoList(Integer currPage, String eventName, String startTime, String endTime) {
+        Integer starIndext = (currPage - 1) * currPage;
+
+
+        return null;
+    }
+
+    /**
+     * 总页数
+     *
+     * @param carNum    当前页
+     * @param eventName 事件名称
+     * @param startTime 条件开始时间
+     * @param endTime   条件结束时间
+     * @return
+     */
+    @Override
+    public Integer getTotalPage(Integer carNum, String eventName, String startTime, String endTime) {
+        return null;
+    }
 }
