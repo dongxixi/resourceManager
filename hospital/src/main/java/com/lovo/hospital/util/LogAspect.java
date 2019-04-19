7593d84476e4e1349aec0dd3f81432d7c67bb9f2
TT <55057163@qq.com>
2019-04-19 09:25:27 +0800

    Merge branch 'master' of E:\github\resourceManager with conflicts.

---
+++ hospital/src/main/java/com/lovo/hospital/util/LogAspect.java
@@ -0,0 +1,76 @@
+package com.lovo.hospital.util;
+
+
+import com.lovo.hospital.entity.UserEntity;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+
+
+@Aspect
+public class LogAspect {
+
+    public Integer id=null;
+
+
+    /**
+     * 管理员登录方法的切入点
+     */
+    @Pointcut("execution(* com.lovo.hospital.service.impl.*.login(..))")
+    public void loginCell(){
+    }
+
+    /**
+     * 添加业务逻辑方法切入点
+     */
+    @Pointcut("execution(* com.lovo.hospital.service.*.save(..))")
+    public void insertCell() {
+    }
+
+    /**
+     * 修改业务逻辑方法切入点
+     */
+    @Pointcut("execution(* com.lovo.hospital.service.*.update(..))")
+    public void updateCell() {
+    }
+
+    /**
+     * 删除业务逻辑方法切入点
+     */
+    @Pointcut("execution(* com.lovo.hospital.service.*.delete(..))")
+    public void deleteCell() {
+    }
+
+    /**
+     * 登录操作(后置通知)
+     * @param joinPoint
+     * @param object
+     * @throws Throwable
+     */
+    @AfterReturning(value = "loginCell()",    returning = "object")
+    public void loginLog(JoinPoint joinPoint, Object object) throws Throwable {
+        UserEntity user=(UserEntity)object;
+        if (user==null) {
+            return;
+        }
+        if (joinPoint.getArgs() == null) {// 没有参数
+            return;
+        }
+         String username=user.getUsername();
+        // 获取方法名
+//        String methodName = joinPoint.getSignature().getName();
+        // 获取操作内容
+//        String opContent = optionContent(joinPoint.getArgs(), methodName);
+
+//        LogEntity log = new LogEntity();
+
+//        logService.insertLog(log);
+
+        System.out.println(username);
+    }
+
+
+
+}
